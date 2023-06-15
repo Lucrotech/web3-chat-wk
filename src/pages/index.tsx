e87cc53f695ce359cb01f69efa88594ae652ca27
Lucrotech <werner@lucrotech.co.za>
2023-06-15 06:32:28 +0200

    Add existing file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Home from '@/components/Home';
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, metamaskWallet, walletConnect } from "@thirdweb-dev/react";
+import { localWallet } from "@thirdweb-dev/react";
 
 export default function Index() {
   return (
-    <ThirdwebProvider activeChain="goerli">
+    <ThirdwebProvider activeChain="goerli" supportedWallets={[metamaskWallet(), walletConnect(), localWallet()]}>
         <Home/>
     </ThirdwebProvider>
   );
