1e5804335e00af3b9cc07154c2b6bf550c0cd318
Lucrotech <werner@lucrotech.co.za>
2023-06-22 17:22:54 +0200

    About page template added.

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+const  = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium nulla in lobortis aliquet. Sed sagittis porttitor vestibulum. Ut dapibus, orci sit amet sodales malesuada, ipsum odio commodo mauris, sit amet feugiat augue est id leo. Etiam tempor sit amet tortor quis fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ac pellentesque orci, quis euismod elit. In eget nisl malesuada, posuere dolor ut, sodales felis. Fusce dui sapien, ornare quis fringilla quis, porttitor vitae lorem. Nam faucibus diam quis nunc malesuada commodo.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium nulla in lobortis aliquet. Sed sagittis porttitor vestibulum. Ut dapibus, orci sit amet sodales malesuada, ipsum odio commodo mauris, sit amet feugiat augue est id leo. Etiam tempor sit amet tortor quis fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ac pellentesque orci, quis euismod elit. In eget nisl malesuada, posuere dolor ut, sodales felis. Fusce dui sapien, ornare quis fringilla quis, porttitor vitae lorem. Nam faucibus diam quis nunc malesuada commodo.</p>
+        </div>
+     );
+}
+ 
+export default ;
